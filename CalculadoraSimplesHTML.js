2fe91b7760d9adfd7cd7ab06fdde69ae3f026607
Brian Windson <60999180+brianwindson@users.noreply.github.com>
2021-11-09 21:05:31 -0300

    js

--- CalculadoraSimplesHTML.js
+++ CalculadoraSimplesHTML.js
@@ -1,7 +1,11 @@
-raiz(){
-    var a = Calculadora.Input.value;
-    var b = parseInt(a);
+const numerosBotoes = document.getElementById('numero')
+const menos = document.getElementById( 'minus')
+const mais = document.getElementById('plus')
+const vezes = document.getElementById('times')
+const div = document.getElementById('div')
+const raiz = document.getElementById('square')
+
+class Calculadora{
     
-    Math.sqrt(a);
-     
 }
+
